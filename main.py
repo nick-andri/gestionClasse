02e38d5119be6052ac3b29a522570d84a7394769
nick <nick.andriambahiny@gmail.com>
2019-08-19 14:40:41 +0200

    ajout test classe eleve

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from PySide2.QtCore import QUrl,QTime,QFileInfo
 import json
 from ui_interface import Ui_Form
 from note import Note
+from matiere import Matiere
+from eleve import Eleve
+
 
 class Fenetre(QWidget):
     def __init__(self,parent = None):
@@ -20,17 +23,65 @@ if __name__ == "__main__":
     filename = "data.json"
     with open(filename,'r') as json_file:
             dico = json.load(json_file)
-
+            # print("dico non modifie")
+            # print(dico)
             # print(dico['academies'][0]['nom'])
             # print(dico['academies'][0]['etablissements'][0]['nom'])
             # print(dico['academies'][0]['etablissements'][0]['classes'][0]['anneeSco'])
-            n = Note(dico['academies'][0]['etablissements'][0]['classes'][0]['eleves'][0]['matieres'][0]['notes'][0])
-            print(n.nom,n.coef,n.valeur)
-            print(n.dico)
-            n.nom='nouveau nom'
-            print(n.nom)
-            n.majDico()
-            print(n.dico)
+
+            ######################################## test objet note #####################################################
+            # n = Note(dico['academies'][0]['etablissements'][0]['classes'][0]['eleves'][0]['matieres'][0]['notes'][0])
+            # print(n.nom,n.coef,n.valeur)
+            # print(n.dico)
+            # n.nom='nouveau nom'
+            # print(n.nom)
+            # n.majDico()
+            # print(n.dico)
+            #
+            # dico['academies'][0]['etablissements'][0]['classes'][0]['eleves'][0]['matieres'][0]['notes'][0]=n.majDico()
+            # print("dico final modifier : ")
+            # print
+            ########################################################## test objet matiere #################################################
+
+            # mat = Matiere(dico['academies'][0]['etablissements'][0]['classes'][0]['eleves'][0]['matieres'][0])
+            #
+            # print(mat.dico)
+            # print(mat.coef)
+            #
+            # # test ecriture et modif
+            # mat.nom = "nouveau nom matiere"
+            # print("dico non modifier :")
+            # print(mat.dico)
+            # # print(mat.nom)
+            # print("dico modifier :")
+            #
+            # dico['academies'][0]['etablissements'][0]['classes'][0]['eleves'][0]['matieres'][0]=mat.majDico()
+            # print("dico objet maj:")
+            # print(mat.dico)
+            # print("dico general maj")
+            # print(dico)
+
+            ######################################  test objet eleves #######################################################
+
+            elf=Eleve(dico['academies'][0]['etablissements'][0]['classes'][0]['eleves'][0])
+            # print(elf.dico)
+            # print(elf.prenom)
+            print(elf.appreciationPP)
+            # test ecriture et modif
+            elf.nom = "nouveau nom eleve"
+            print("dico non modifier :")
+            print(elf.dico)
+
+            print("dico modifier :")
+
+            dico['academies'][0]['etablissements'][0]['classes'][0]['eleves'][0]=elf.majDico()
+            print("dico objet maj:")
+            print(elf.dico)
+            print("dico general maj")
+            print(dico)
+
+
+
     # window = Fenetre()
     # window.show()
     #
